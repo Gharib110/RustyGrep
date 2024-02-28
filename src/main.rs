abcc1f5dbffdf69d8dd408fa0a9acbc1ed263e2b
AlirezaGharib <alirezagharib110@gmail.com>
2024-02-29 00:08:14 +0330

    Manipulating the structure of program

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,34 @@
 use std::env::args;
 use std::fs::File;
 use std::io::Read;
+use std::ops::Bound::Included;
+
+struct InputArgs<'a> {
+    query: &'a String,
+    filename: &'a String
+}
 
 fn main() {
     let args:Vec<String> = args().collect();
 
-    let query = &args[0];
-    let filename = &args[1];
-
-    let mut f = File::open(filename).expect("File Not Found !");
+    let config: InputArgs = InputArgs::new(&args);
+    let mut f = File::open(config.filename).expect("File Not Found !");
     let mut contents: String = String::new();
 
     f.read_to_string(&mut contents).expect(
         "Something Went Wrong During the reading of the File Contents");
 
 
+}
+
+impl InputArgs {
+    fn new(args: &Vec<String>) -> InputArgs {
+        let query = &args[0];
+        let filename = &args[1];
+
+        return InputArgs{
+            query,
+            filename,
+        };
+    }
 }
