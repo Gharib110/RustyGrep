5459913ee3af733038cd249e493e57effe8d2f84
AlirezaGharib <alirezagharib110@gmail.com>
2024-02-28 23:57:26 +0330

    Reading input args and file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::env::args;
+use std::fs::File;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    let args:Vec<String> = args().collect();
+
+    let query = &args[0];
+    let filename = &args[1];
+
+    let mut f = File::open(filename).expect("File Not Found !");
+    let mut contents: String = String::new();
+
+    f.read_to_string(&mut contents).expect(
+        "Something Went Wrong During the reading of the File Contents");
+
+
 }
