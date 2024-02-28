ceeac43928922b1cb27ea845c57660b42b3511be
AlirezaGharib <alirezagharib110@gmail.com>
2024-02-29 00:21:02 +0330

    Organizing the Error Handling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env::args;
 use std::fs::File;
 use std::io::Read;
-use std::ops::Bound::Included;
+use std::process;
 
 struct InputArgs<'a> {
     query: &'a String,
@@ -11,8 +11,18 @@ struct InputArgs<'a> {
 fn main() {
     let args:Vec<String> = args().collect();
 
-    let config: InputArgs = InputArgs::new(&args);
-    let mut f = File::open(config.filename).expect("File Not Found !");
+    let config:Result<InputArgs, &str> = InputArgs::new(&args);
+    let mut conf: InputArgs;
+    match config {
+        Ok(t) => {
+            conf = t;
+        }
+        Err(err) => {
+            println!("{err}");
+            process::exit(1);
+        }
+    }
+    let mut f = File::open(conf.filename).expect("File Not Found !");
     let mut contents: String = String::new();
 
     f.read_to_string(&mut contents).expect(
@@ -21,14 +31,17 @@ fn main() {
 
 }
 
-impl InputArgs {
-    fn new(args: &Vec<String>) -> InputArgs {
+impl<'a> InputArgs<'a> {
+    fn new(args: &Vec<String>) -> Result<InputArgs, &str> {
+        if args.len() < 3 {
+            return Err("Not Enough Arguments !");
+        }
         let query = &args[0];
         let filename = &args[1];
 
-        return InputArgs{
+        Ok(InputArgs{
             query,
             filename,
-        };
+        })
     }
 }
